2a2c3a73337b5e212f5755eec77a88353508735d
Hannah Ramadan <hannahr315@gmail.com>
2024-07-24 11:50:18 -0700

    test: add close sql query

--- instrumentation/pg/test/opentelemetry/instrumentation/pg/instrumentation_test.rb
+++ instrumentation/pg/test/opentelemetry/instrumentation/pg/instrumentation_test.rb
@@ -268,7 +268,7 @@ describe OpenTelemetry::Instrumentation::PG::Instrumentation do
     end
 
     it 'extracts table name' do
-      client.query('CREATE TABLE test_table (id SERIAL PRIMARY KEY, name VARCHAR(50)')
+      client.query('CREATE TABLE test_table (id SERIAL PRIMARY KEY, name VARCHAR(50);')
 
       _(span.attributes['db.collection.name']).must_equal 'table_name'
     end
